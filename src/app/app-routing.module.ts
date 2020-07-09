b013b039793f8e89c46f1b813771bca505e31374
Gandreti <Praveen.KumarG@Chubb.com>
2020-07-09 17:09:03 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,50 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
+import { BubbleChartComponent } from './bubble-chart/bubble-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
+import{PolarAreaChartComponent} from './polar-area-chart/polar-area-chart.component'
+import{DynamicChartComponent}from './dynamic-chart/dynamic-chart.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: "line-chart",
+    component: LineChartComponent
+  },
+  {
+    path: "bar-chart",
+    component: BarChartComponent
+  },
+  {
+    path: "doughnut-chart",
+    component: DoughnutChartComponent
+  },
+  {
+    path: "bubble-chart",
+    component: BubbleChartComponent
+  },
+  {
+    path: "pie-chart",
+    component: PieChartComponent
+  },
+  {
+    path: "radar-chart",
+    component: RadarChartComponent
+  },
+  {
+    path: "dynamic-chart",
+    component: DynamicChartComponent
+  },
+  {
+    path: "polar-area-chart",
+    component: PolarAreaChartComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+import { BubbleChartComponent } from './bubble-chart/bubble-chart.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { RadarChartComponent } from './radar-chart/radar-chart.component';
+import { DoughnutChartComponent } from './doughnut-chart/doughnut-chart.component';
+import { ChartsModule } from 'ng2-charts';
+import { DynamicChartComponent } from './dynamic-chart/dynamic-chart.component';
+import { PolarAreaChartComponent } from './polar-area-chart/polar-area-chart.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartComponent,
+    BubbleChartComponent,
+    LineChartComponent,
+    PieChartComponent,
+    RadarChartComponent,
+    DoughnutChartComponent,
+    DynamicChartComponent,
+    PolarAreaChartComponent
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
